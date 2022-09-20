440a55aa8fb1752845b1f14dfec25a3e249d8d7e
Rodrigo Causarano <rjcausarano@gmail.com>
2022-09-20 19:38:47 -0400

    passing in arguments

--- manipulator_gazebo/src/pose_calculator.cpp
+++ manipulator_gazebo/src/pose_calculator.cpp
@@ -1,15 +1,18 @@
 #include "manipulator_gazebo/pose_calculator.hpp"
 #include <string>
-#include <chrono>
-
-using namespace std::chrono_literals;
 
 PoseCalculator::PoseCalculator()
 : Node("pose_calculator")
 {
     publisher_ = this->create_publisher<manipulator_msgs::msg::Pose>("set_pose", 10);
-    pose_msg_.data = {1, 1, 1};
-    publisher_->publish(pose_msg_);
+    short unsigned int x, y, z;
+    while(true){
+        std::cin >> x;
+        std::cin >> y;
+        std::cin >> z;
+        pose_msg_.data = {x, y, z};
+        publisher_->publish(pose_msg_);
+    }
 }
 
 int main(int argc, char **argv)
